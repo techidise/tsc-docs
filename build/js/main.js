fb44fa40e6e0c17fd5ebd20c7c32ab3936d6841c
TechiDise <info@techidise.com>
2025-12-20 13:31:38 -0800

    Added "longerArray" and "longerString"

--- build/js/main.js
+++ build/js/main.js
@@ -22,3 +22,13 @@ const longest = (a, b) => {
         return b;
     }
 };
+// longerArray is of type 'number[]'
+const longerArray = longest([1, 2], [1, 2, 3]);
+console.log(longerArray);
+// longerString is of type 'alice' | 'bob'
+const longerString = longest('alice', 'bob');
+console.log(longerString);
+// Error! Numbers don't have a 'length' property
+// const notOK = longest(10, 100);
+// console.log(notOK);
+//* Working w/ Contrained Values *\\
--- src/main.ts
+++ src/main.ts
@@ -29,3 +29,17 @@ const longest = <T extends { length: number }>(a: T, b: T) => {
     return b;
   }
 };
+
+// longerArray is of type 'number[]'
+const longerArray = longest([1, 2], [1, 2, 3]);
+console.log(longerArray);
+
+// longerString is of type 'alice' | 'bob'
+const longerString = longest('alice', 'bob');
+console.log(longerString);
+
+// Error! Numbers don't have a 'length' property
+// const notOK = longest(10, 100);
+// console.log(notOK);
+
+//* Working w/ Contrained Values *\\
